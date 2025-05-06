b0d733891e55b6d929e2a897aa7baf2fb90df857
Icaro Borges <125396165+icaro9293@users.noreply.github.com>
2025-05-06 17:38:02 -0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
         //z-50 é para o navbar não ficar atras das outras camadas quando dar scroll
         <div className='w-full md:h-12 sm:h-14 h-18 bg-white dark:bg-gray-900 transition-colors duration-500 flex flex-col md:flex-row justify-center md:justify-between items-center xl:px-36 lg:px-24 md:px-12 sm:px-6 px-4 fixed top-0 z-50'>
             <div className='flex items-center sm:gap-x-4 gap-x-2'>
-                <a href="#" className='md:text-2xl sm:text-xl text-lg '>Clone Lion</a>
+                <a href="#" className='md:text-2xl sm:text-xl text-lg '>Icaro Moreira</a>
                 {/* bx é do box icon || sun é o nome do ícone*/}
                 <i
                     className={`${darkMode ? 'bx bx-sun' : 'bx bx-moon'}  md:text-3xl sm:text-2xl text-xl text-gray-600 dark:text-gray-200 sm:ml-4 ml-2 cursor-pointer`}
